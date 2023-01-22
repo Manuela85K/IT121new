fa094481e7b534ef0fd4c165f87456b516455215
ManuelaKay <104327037+Manuela85K@users.noreply.github.com>
2023-01-22 23:13:31 +0000

    code

--- index.js
+++ index.js
@@ -9,21 +9,21 @@
 
   for (let i = 0; i < words.length; i++) {
     let word = words[i];
-    let firstLetter = word[0];
-    let secondLetter = word[1];
+    let letterOne = word[0];
+    let letterTwo = word[1];
 
-    if (firstLetter === "a","e", "i", "o", "u") {
-      pigLatin += word + "way ";
-    } else if(secondLetter) {
-      pigLatin += word.slice(1) + firstLetter + "ay ";
+    if (letterOne = "a","e", "i", "o", "u") {
+      pigLatin = word + "way ";
+    } else if(!letterTwo) {
+      pigLatin = word.slice(0,1) + letterOne + "ay ";
     } else {
-      pigLatin += word.slice(2) + firstLetter + secondLetter + "ay ";
+      pigLatin = word.slice(-2) + letterOne + letterTwo + "ay ";
     }
   }
   pigLatin = pigLatin.slice(0, -1);
   return pigLatin;
 }
-console.log(translateToPigLatin("Manuela Hello"));
+console.log(translateToPigLatin("Lion Mane"));
 console.log(translateToPigLatin("America"))
 console.log(translateToPigLatin("")); 
 console.log(translateToPigLatin("L")); 
